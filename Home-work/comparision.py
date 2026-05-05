70ef20b665ef76c87e60f4884defb3b1381a6b92
kaushalchauhan84850 <kaushalchauhan84850@gmail.com>
2026-05-05 09:44:54 +0530

    hw

---
+++ Home-work/comparision.py
@@ -0,0 +1,15 @@
+a = int(input("Enter the first number: "))
+b = int(input("enter the second number: "))
+
+# val = a > b
+# print(f"{val}")
+# val = a < b
+# print(f"{val}")
+# val = a == b
+# print(f"{val}")
+# val = a != b
+# print(f"{val}")
+# val = a >= b
+# print(f"{val}")
+val = a <= b
+print(f"{val}")
