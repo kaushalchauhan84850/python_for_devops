54b5bdfcd4b686d6ad256d5ecc1899fdba0c31b5
kaushalchauhan84850 <kaushalchauhan84850@gmail.com>
2026-05-06 10:46:59 +0530

    todays class

---
+++ 06-05-2026/morning-class.py
@@ -0,0 +1,92 @@
+# a = 0
+# if a:
+#     print("yes")
+# else:
+#     print("no")
+ 
+# # in python 0 is considered as false and any other number is considered as true
+# # and empty string is considered as false and any other string is considered as true
+# # and the true and false are also considered as true and false respectively
+
+
+# a = "aman"
+# if a:
+#     print("yes")
+# else:
+#     print("no")    
+    
+    
+    
+# # we can also use the membership operator in python to find the value in the string
+# # for example
+# str1 = "hello world"
+# find = input("Enter the number or word you want to find:")
+# print(find in str1)
+
+# # we can also perform arthematic operations on true and false values
+# print(True + False)  # Output: 1
+# print(True * False)  # Output: 0 
+# print (True - False)  # Output: 1
+# print (False - True)  # Output: -1
+
+
+# c = True
+# print(type(c))
+
+# if 1*1*0:
+#     print("yes")
+# else:    
+#     print("no")
+    
+    
+    
+
+# # WAP to find weather the input is odd or even
+
+# number = int(input("Enter a number:"))
+# if number % 2 == 0:
+#     print(f"{number} is an even number")
+# else:
+#     print(f"{number} is an odd number")
+    
+    
+# WAP to print the last digit of the number is 456735
+
+# last_digit = 45670003 % 1000  
+# print(last_digit)
+
+
+# we can use indexing to find the last character of the string
+# for example
+
+# name = "karan"
+# print(name[-1])
+
+#wap to find largest and smallest 
+num1 = 3
+num2 = 9
+num3 = 12
+
+if num1 >num2 and num1>num3:
+  print(f"{num1}is the gratest among 3" )
+elif num2>num1 and num2>num3:
+   print(f"{num2} is the gratest among 3")
+else :
+    print(f"{num3} is gratest ")     
+
+# for the smallest number
+
+if num1<num2 and num1<num2:
+    print(f"{num1} is the chotu ")
+elif num2<num1 and num2<num3:
+    print(f"{num2} ye hai chotu") 
+else:
+    print("mil gaya chotu")
+    
+    
+    
+    
+# Conditional statements
+# 1 if statement
+# 2 if else also known as elif statement
+# nested if statement 
